3203d38c6071ededaeadcc2e4b7e2fadfaaf372a
Ярослав Нестеров <atomofiron@yandex.ru>
2017-08-26 02:04:55 +0300

    atypical fab properties fix

--- app/src/main/java/io/atomofiron/tolmach/custom/VoicePowerIndicator.java
+++ app/src/main/java/io/atomofiron/tolmach/custom/VoicePowerIndicator.java
@@ -2,11 +2,13 @@ package io.atomofiron.tolmach.custom;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.res.ConfigurationHelper;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,17 @@ import android.view.animation.Animation;
 
 import io.atomofiron.tolmach.R;
 
+import static android.support.design.widget.FloatingActionButton.SIZE_AUTO;
+import static android.support.design.widget.FloatingActionButton.SIZE_MINI;
+import static android.support.design.widget.FloatingActionButton.SIZE_NORMAL;
+
 /* костыль... или инновационное решение*/
 public class VoicePowerIndicator extends View implements Animation.AnimationListener, ValueAnimator.AnimatorUpdateListener {
 	private Paint paint;
 	private FloatingActionButton fab;
 	private ValueAnimator animator = ValueAnimator.ofFloat();
 
+	private int realFabSize;
 	private int fabRadius;
 	private float scale = 0f;
 
@@ -76,13 +83,32 @@ public class VoicePowerIndicator extends View implements Animation.AnimationList
 	}
 
 	private void calculate() {
-		fabRadius = fab.getWidth() / 2;
+		fabRadius = realFabSize / 2;
 		ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) fab.getLayoutParams();
 
 		// работает пока только, если FAB в такой конфигурации
-		cx = -fabRadius - mlp.rightMargin;
-		cy = -fabRadius - mlp.bottomMargin;
+		cx = -fabRadius - mlp.rightMargin - (fab.getWidth() - realFabSize) / 2;
+		cy = -fabRadius - mlp.bottomMargin - (fab.getHeight() - realFabSize) / 2;
 		maxRadius = fabRadius * 5;
+	}
+
+	/** @see android.support.design.widget.FloatingActionButton#getSizeDimension(int) */
+	private int getSizeDimension(@FloatingActionButton.Size final int size) {
+		final Resources res = getResources();
+		switch (size) {
+			case SIZE_AUTO:
+				// If we're set to auto, grab the size from resources and refresh
+				final int width = ConfigurationHelper.getScreenWidthDp(res);
+				final int height = ConfigurationHelper.getScreenHeightDp(res);
+				return Math.max(width, height) < 470
+						? getSizeDimension(SIZE_MINI)
+						: getSizeDimension(SIZE_NORMAL);
+			case SIZE_MINI:
+				return res.getDimensionPixelSize(android.support.design.R.dimen.design_fab_size_mini);
+			case SIZE_NORMAL:
+			default:
+				return res.getDimensionPixelSize(android.support.design.R.dimen.design_fab_size_normal);
+		}
 	}
 
 	private void startAnim(float nextScale) {
@@ -93,6 +119,7 @@ public class VoicePowerIndicator extends View implements Animation.AnimationList
 
 	public void setFAB(FloatingActionButton fab) {
 		this.fab = fab;
+		realFabSize = getSizeDimension(fab.getSize());
 	}
 
 	@Override
